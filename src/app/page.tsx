1033c1455b4635a2bb5bb6a5ccb1a344038f0f24
Mishalia Pillay <Mishalia.Pillay@boxfusion.io>
2025-03-08 15:42:56 +0200

    fixing vercel deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Page() {
-  return <h1>Hello Next.js!</h1>;
+  return (
+    <>
+      <h1>Hello Next.js!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste qui
+        obcaecati aliquam tenetur aperiam excepturi ipsum, in harum sequi.
+        Nostrum voluptas distinctio illo ipsa harum ex quod non aut maiores?
+      </p>
+    </>
+  );
 }
